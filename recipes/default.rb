042278bf18ac4fd16d9c326a440294aeef88b8ce
Chris Bloom <chrisbloom7@gmail.com>
2015-01-19 17:37:05 -0500

    Create new template from template1

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,7 +26,7 @@ end
 
 execute "create_postgis_template" do
   user "postgres"
-  command "createdb -E UTF8 #{node['postgis']['template_name']} -T template0"
+  command "createdb -E UTF8 #{node['postgis']['template_name']} -T template1"
   not_if "psql -qAt --list | grep '^#{node['postgis']['template_name']}\|'", user: "postgres"
   action :run
 end
